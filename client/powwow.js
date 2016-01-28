aee7f4ce50eca50b77699a7a1b363173569bc97c
Cajulis Manry, Amber <amber.manry@icfi.com>
2016-01-27 23:40:41 -0500

    wip, insert data into mongo on submit

--- client/powwow.js
+++ client/powwow.js
@@ -1,5 +1,65 @@
 Template.powwow.onRendered(function() {
-    //this.$('.datetimepicker').datetimepicker();
+    //create time picker
     $('#startTime').lolliclock({autoclose:true});
     $('#endTime').lolliclock({autoclose:true});
 });
+
+Template.powwow.events({
+    'click .submit': function(event){
+        event.preventDefault();
+        console.log("submit clicked");
+        Suggestions.insert({
+            place: $("#place").val(),
+            startTime: $("#startTime").val(),
+            endTime: $("#endTime").val(),
+            notes: $("#notes").val(),
+            createdAt: new Date(),
+            participants : [prompt("Who are you?")]
+        });
+    }
+});
+
+//$('.ui.form')
+//    .form({
+//        fields: {
+//            startTime     : 'empty',
+//            endTime   : 'empty',
+//            place : 'empty'
+//        }
+//    })
+//;
+
+//$('.ui.form')
+//    .form({
+//        on: 'blur',
+//        fields: {
+//            startTime: {
+//                identifier  : 'startTime',
+//                rules: [
+//                    {
+//                        type   : 'empty',
+//                        prompt : 'Please enter start time'
+//                    }
+//                ]
+//            },
+//            endTime: {
+//                identifier  : 'endTime',
+//                rules: [
+//                    {
+//                        type   : 'empty',
+//                        prompt : 'Please enter an end time'
+//                    }
+//                ]
+//            },
+//            place: {
+//                identifier  : 'place',
+//                rules: [
+//                    {
+//                        type   : 'empty',
+//                        prompt : 'Please enter a location'
+//                    }
+//                ]
+//            }
+//        }
+//    })
+//;
--- hungr-semantic-ui.js
+++ hungr-semantic-ui.js
@@ -1,4 +1,5 @@
-
+// Define a collection to hold our suggestions
+Suggestions = new Mongo.Collection("suggestions");
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
