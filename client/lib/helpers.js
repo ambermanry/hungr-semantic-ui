d7ebd7b37aacf2c7e364e1b5a02dd7679d0a86e7
Cajulis Manry, Amber <amber.manry@icfi.com>
2016-01-28 21:35:51 -0500

    only display today's entries

--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -1,7 +1,12 @@
 if(Meteor.isClient){
 	Template.lunchroom.helpers({
     		'events' : function () {
-        		return Events.find({}, {sort: {startTime: 1}});
+                var start = new Date();
+                start.setHours(0,0,0,0);
+                var end = new Date();
+                end.setHours(23,59,59,999);
+                return Events.find({"createdAt":{ "$gte":start, "$lt":end}},{sort: {startTime: 1}});
+        		//return Events.find({}, {sort: {startTime: 1}});
     	}
 	});
 }
