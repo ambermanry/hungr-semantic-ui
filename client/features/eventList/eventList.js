c8bcc9b1d381b1d1540a8c95a373fa96e1a85111
Cajulis Manry, Amber <amber.manry@icfi.com>
2016-01-29 13:27:19 -0500

    added join functionality

--- client/features/eventList/eventList.js
+++ client/features/eventList/eventList.js
@@ -2,5 +2,14 @@ Template.eventList.events({
     "click .icon.delete": function (event) {
         event.preventDefault();
         Events.remove(event.target.getAttribute("data-id"));
+    },
+    "click .ui.button.join": function (event) {
+        var eventId = event.target.getAttribute("data-id");
+        var newParticipant = prompt("Who are you?");
+        if(newParticipant != null) {
+            var participants = Events.find({_id: eventId}).fetch()[0].participants;
+            participants.push(newParticipant);
+            Events.update(eventId, {$set :{participants : participants}});
+        }
     }
 });
