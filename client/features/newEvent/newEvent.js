97cebdc9b6876cc74a863b6fb13bc7a9953e94b3
Cajulis Manry, Amber <amber.manry@icfi.com>
2016-02-01 22:09:26 -0500

    use modal to create new event

--- client/features/newEvent/newEvent.js
+++ client/features/newEvent/newEvent.js
@@ -54,29 +54,67 @@ Template.newEvent.rendered = function () {
     $(document)
         .on("submit", "form[name='newEventForm']", function (event) {
             event.preventDefault();
-            console.log('working');
-            console.log("startTime:" + timeToDate($("#startTime").val()));
-            var newParticipant = [prompt("Who are you?")];
-            var joinDate = new Date();
-            var newComment = {user: newParticipant, date: joinDate, displayDate: dateToTime(joinDate), canModify: false, message: newParticipant + " created event at: " + dateToTime(joinDate)};
-            Events.insert({
-                place: $("#place").val(),
-                startTime: timeToDate($("#startTime").val()),
-                startTimeDisplay: $("#startTime").val(),
-                endTime: timeToDate($("#endTime").val()),
-                endTimeDisplay: $("#endTime").val(),
-                notes: $("#notes").val(),
-                createdAt: new Date(),
-                participants : newParticipant,
-                comments: [newComment]
-            });
-            if (Session.get("userType")=="guest") {
-                Session.set("displayName", newParticipant);
+            var newParticipant = Session.get("displayName");
+            if (Session.get("userType") == "guest") {
+                $('.modal[name="displayNameModal"]').modal({
+                    onDeny : function () {
+                        console.log("cancel");
+                        $('.modal[name="displayNameModal"]').modal('hide');
+                    },
+                    onApprove : function () {
+                        console.log("yes, entered user name:" + $("#newDisplayName").val());
+                        newParticipant = $("#newDisplayName").val();
+                        // create new event
+                        // = [prompt("Who are you?")];
+                        var joinDate = new Date();
+                        var newComment = {user: newParticipant, date: joinDate, displayDate: dateToTime(joinDate), canModify: false, message: newParticipant + " created event at: " + dateToTime(joinDate)};
+                        Events.insert({
+                            place: $("#place").val(),
+                            startTime: timeToDate($("#startTime").val()),
+                            startTimeDisplay: $("#startTime").val(),
+                            endTime: timeToDate($("#endTime").val()),
+                            endTimeDisplay: $("#endTime").val(),
+                            notes: $("#notes").val(),
+                            createdAt: new Date(),
+                            participants : newParticipant,
+                            comments: [newComment]
+                        });
+                        Session.set("displayName", newParticipant);
+                        //clear form
+                        $("#place").val("");
+                        $("#startTime").val("");
+                        $("#endTime").val("");
+                        $("#notes").val("");
+                        $('.modal[name="displayNameModal"]').modal('hide');
+                    }
+                })
+                    .modal('show');
+            } else { //we are logged in
+                var joinDate = new Date();
+                var newComment = {user: newParticipant, date: joinDate, displayDate: dateToTime(joinDate), canModify: false, message: newParticipant + " created event at: " + dateToTime(joinDate)};
+                Events.insert({
+                    place: $("#place").val(),
+                    startTime: timeToDate($("#startTime").val()),
+                    startTimeDisplay: $("#startTime").val(),
+                    endTime: timeToDate($("#endTime").val()),
+                    endTimeDisplay: $("#endTime").val(),
+                    notes: $("#notes").val(),
+                    createdAt: new Date(),
+                    participants : newParticipant,
+                    comments: [newComment]
+                });
+                if (Session.get("userType")=="guest") {
+                    Session.set("displayName", newParticipant);
+                }
+                //clear form
+                $("#place").val("");
+                $("#startTime").val("");
+                $("#endTime").val("");
+                $("#notes").val("");
+                $('.modal[name="displayNameModal"]').modal('hide');
             }
-            //clear form
-            $("#place").val("");
-            $("#startTime").val("");
-            $("#endTime").val("");
-            $("#notes").val("");
+
+
+
     });
 };
