4e104266f85841ebfd104402cb73a74f671fc4e3
Cajulis Manry, Amber <amber.manry@icfi.com>
2016-02-02 22:58:44 -0500

    load google maps js

--- client/lib/googlePlaces.js
+++ client/lib/googlePlaces.js
@@ -1,8 +1,3 @@
-GoogleMaps.init({
-    'key': 'AIzaSyBIaUQmTt_4jtu8zFDI2n4bDz3aBKzwx-0', //optional
-    'language': 'en',  //optional
-    'libraries': 'places'
-});
 
 window.onload = function() {
     var autocomplete = new google.maps.places.Autocomplete(
